4841a70818bd074039239c616910fd9d9d57fac3
Arun <arun@mobilitys-Macintosh.local>
2016-04-07 19:04:02 +0530

    Adding CoreData Protection

---
+++ ProtectionCD/ProtectionCD/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ProtectionCD
+//
+//  Created by Arun on 3/3/16.
+//  Copyright © 2016 Arun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ProtectionCD/ProtectionCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProtectionCD
+//
+//  Created by Arun on 3/3/16.
+//  Copyright © 2016 Arun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
