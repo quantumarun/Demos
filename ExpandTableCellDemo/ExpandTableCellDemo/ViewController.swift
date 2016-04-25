f05fd5fecaa97c8a8cac18afec2e4fdfa3500396
Arun <arun@t001765fd8dff.vtc.csc.com>
2016-04-25 12:27:07 +0530

    Expandable UITableViewCell

---
+++ ExpandTableCellDemo/ExpandTableCellDemo/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  ExpandTableCellDemo
+//
+//  Created by Arun on 4/25/16.
+//  Copyright © 2016 Arun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var expandTableView: UITableView!
+    var selectedIndex = -1
+    var dataArray : [[String:String]] = [["MatchName" : "First Match", "Team1" : "US", "Team2" : "Australia", "Score" : "Score 1-0"], ["MatchName" : "Second Match", "Team1" : "India", "Team2" : "Sri lanka", "Score" : "Score 2-0"], ["MatchName" : "Third Match", "Team1" : "West Indies", "Team2" : "South Africa", "Score" : "Score 2-4"], ["MatchName" : "Fourth Match", "Team1" : "New Zealand", "Team2" : "Holland", "Score" : "Score 0-0"] ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as! customCell
+        
+        let obj = dataArray[indexPath.row]
+        cell.matchName.text = obj["MatchName"]
+        if(selectedIndex == indexPath.row) {
+            cell.heightConstraintSecondView.constant = 0
+        } else {
+            cell.heightConstraintSecondView.constant = 59
+            cell.team1.text = obj["Team1"]
+            cell.team2.text = obj["Team2"]
+            cell.score.text = obj["Score"]
+        }
+        return cell;
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if(selectedIndex == indexPath.row) {
+            return 100;
+        } else {
+            return 40;
+        }
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        selectedIndex = indexPath.row
+        self.expandTableView.beginUpdates()
+        self.expandTableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic )
+        self.expandTableView.endUpdates()
+    }
+
+}
+
---
+++ ExpandTableCellDemo/ExpandTableCellDemo/customCell.swift
@@ -0,0 +1,31 @@
+//
+//  customCell.swift
+//  ExpandTableCellDemo
+//
+//  Created by Arun on 4/25/16.
+//  Copyright © 2016 Arun. All rights reserved.
+//
+
+import UIKit
+
+class customCell: UITableViewCell {
+
+    @IBOutlet weak var matchName: UILabel!
+    @IBOutlet weak var team1: UILabel!
+    @IBOutlet weak var team2: UILabel!
+    @IBOutlet weak var score: UILabel!
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var heightConstraintSecondView: NSLayoutConstraint!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
