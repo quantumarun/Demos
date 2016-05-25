7298d0e09bc17a8c08f26b1d3478d1137cb2de8a
Arun <arun@mobilitys-Macintosh.local>
2016-05-25 14:52:41 +0530

    Part 2:Self Sizing UITextView Sample with scrolling

--- AdaptiveUITextView/AdaptiveUITextView/ViewController.swift
+++ AdaptiveUITextView/AdaptiveUITextView/ViewController.swift
@@ -10,18 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textEntry: UITextView!
     
+    @IBOutlet weak var textHeightConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func dismissKeyboard(sender: UIButton) {
+        self.textEntry.resignFirstResponder()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension ViewController: UITextViewDelegate {
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        print("Content Height \(self.textEntry.contentSize.height) ")
+        if(self.textEntry.contentSize.height < self.textHeightConstraint.constant) {
+            self.textEntry.scrollEnabled = false
+        } else {
+            self.textEntry.scrollEnabled = true
+        }
+        
+        return true
+    }
 }
 
 
