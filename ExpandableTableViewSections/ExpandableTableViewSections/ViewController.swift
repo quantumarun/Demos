64a8cc3cf13b18e0f72619c2ecb83d741b300b62
Arun Gupta <arun@Aruns-MacBook-Pro.local>
2017-03-17 06:51:58 +0530

    Added didSelectRow Handler to prompt for adding quantities using alert view

--- ExpandableTableViewSections/ExpandableTableViewSections/ViewController.swift
+++ ExpandableTableViewSections/ExpandableTableViewSections/ViewController.swift
@@ -17,6 +17,11 @@ class ViewController: UIViewController, UITableViewDataSource {
     var row2 = ["Mango", "Orange", "Watermelon", "Apple", "Apricot", "Banana", "Papaya", "Pineapple", "Melon", "Avocado", "Cherry", "Date", "Fig", "Grape", "Guava", "Kiwi", "Olive", "Pear"]
     var row3 = ["Parrot", "Peacock", "Woodpecker", "Kingfisher", "Owl", "Eagle", "Pigeon", "Vulture", "Bats", "Nightingale", "Crow"]
     var row4 = ["Lizard", "Crocodile", "Snake", "Turtle", "Dinosaur"]
+    
+    var row1Q : [Int] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var row2Q : [Int] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var row3Q : [Int] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var row4Q : [Int] = [0, 0, 0, 0, 0]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -41,7 +46,7 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
 }
 
-extension ViewController: UITableViewDelegate {
+extension ViewController: UITableViewDelegate, UITextFieldDelegate {
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return sectionData.count
@@ -97,17 +102,75 @@ extension ViewController: UITableViewDelegate {
         let cellIdentifier = "Cell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
         
+        cell?.selectionStyle = .none
         switch indexPath.section {
         case 0:
-            cell?.textLabel?.text = row1[indexPath.row]
+            cell?.textLabel?.text = "\(row1[indexPath.row]) : \(row1Q[indexPath.row])"
         case 1:
-            cell?.textLabel?.text = row2[indexPath.row]
+            cell?.textLabel?.text = "\(row2[indexPath.row]) : \(row2Q[indexPath.row])"
         case 2:
-            cell?.textLabel?.text = row3[indexPath.row]
+            cell?.textLabel?.text = "\(row3[indexPath.row]) : \(row3Q[indexPath.row])"
         default:
-            cell?.textLabel?.text = row4[indexPath.row]
+            cell?.textLabel?.text = "\(row4[indexPath.row]) : \(row4Q[indexPath.row])"
         }
         return cell!;
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var item = ""
+        switch indexPath.section {
+        case 0:
+            item = row1[indexPath.row]
+        case 1:
+            item = row2[indexPath.row]
+        case 2:
+            item = row3[indexPath.row]
+        default:
+            item = row4[indexPath.row]
+        }
+        
+        let msgString = "Enter the number of quantities for \(item)"
+        let alertController: UIAlertController = UIAlertController(title: "Add", message: msgString, preferredStyle: .alert)
+        
+        //cancel button
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .cancel) { action -> Void in
+            //cancel code
+        }
+        alertController.addAction(cancelAction)
+        
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default, handler: {
+            alert -> Void in
+            
+            let firstTextField = alertController.textFields![0] as UITextField
+            
+            print("firstName \(firstTextField.text)")
+            
+            
+            if ((firstTextField.text?.characters.count)! > 0) {
+            switch indexPath.section {
+            case 0:
+                self.row1Q[indexPath.row] = Int(firstTextField.text!)!
+            case 1:
+                self.row2Q[indexPath.row] = Int(firstTextField.text!)!
+            case 2:
+                self.row3Q[indexPath.row] = Int(firstTextField.text!)!
+            default:
+                self.row4Q[indexPath.row] = Int(firstTextField.text!)!
+            }
+            
+            self.expandTableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+        })
+        alertController.addAction(saveAction)
+        
+        //Add text field
+        alertController.addTextField { (textField) -> Void in
+            //textField.textColor = UIColor.green
+        }
+        //Present the AlertController
+        present(alertController, animated: true, completion: nil)
+
     }
 }
 
