6a0d9ddc9a5ca7d06a10e302df30d88028cd6341
Arun <arun.h.gupta@capgemini.com>
2017-05-08 22:34:59 +0530

    Added MiliSeconds to the timer

--- TimerApp/TimerApp/ViewController.swift
+++ TimerApp/TimerApp/ViewController.swift
@@ -15,9 +15,11 @@ class ViewController: UIViewController {
     var hrs = 0
     var min = 0
     var sec = 0
+    var milliSecs = 0
     var diffHrs = 0
     var diffMins = 0
     var diffSecs = 0
+    var diffMilliSecs = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,12 +28,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func startBtnPressed(_ sender: UIButton) {
-        self.removeSavedDate()
-        self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateLabels(t:))), userInfo: nil, repeats: true)
+        self.resetContent()
+        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: (#selector(ViewController.updateLabels(t:))), userInfo: nil, repeats: true)
     }
     
     @IBAction func resetBtnPressed(_ sender: UIButton) {
-        self.removeSavedDate()
         self.resetContent()
     }
     
@@ -54,25 +55,31 @@ class ViewController: UIViewController {
     }
     
     func resetContent() {
+        self.removeSavedDate()
         timer.invalidate()
-        self.stopWatchLbl.text = "00 : 00 : 00"
+        self.stopWatchLbl.text = "00 : 00 : 00 : 00"
+        self.milliSecs = 0
         self.sec = 0
         self.min = 0
         self.hrs = 0
     }
     
     func updateLabels(t: Timer) {
-        if (self.sec == 59) {
-                self.min += 1
-                self.sec = 0
-                if (self.min == 60) {
-                    self.hrs += 1
-                    self.min = 0
-                }
-        } else {
+        if(self.milliSecs == 59) {
             self.sec += 1
+            self.milliSecs = 0
+            if (self.sec == 60) {
+                    self.min += 1
+                    self.sec = 0
+                    if (self.min == 60) {
+                        self.hrs += 1
+                        self.min = 0
+                    }
+            }
+        } else {
+            self.milliSecs += 1
         }
-        self.stopWatchLbl.text = String(format: "%02d : %02d : %02d", self.hrs, self.min, self.sec)
+        self.stopWatchLbl.text = String(format: "%02d : %02d : %02d : %02d", self.hrs, self.min, self.sec, self.milliSecs)
     }
     
     static func getTimeDifference(startDate: Date) -> (Int, Int, Int) {
@@ -85,8 +92,8 @@ class ViewController: UIViewController {
         self.hrs += hours
         self.min += mins
         self.sec += secs
-        self.stopWatchLbl.text = String(format: "%02d : %02d : %02d", self.hrs, self.min, self.sec)
-        self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateLabels(t:))), userInfo: nil, repeats: true)
+        self.stopWatchLbl.text = String(format: "%02d : %02d : %02d : %02d", self.hrs, self.min, self.sec, self.milliSecs)
+        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: (#selector(ViewController.updateLabels(t:))), userInfo: nil, repeats: true)
         
     }
     
